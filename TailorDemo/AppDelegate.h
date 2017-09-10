7750ac5e593f54b43ec34729a5d7a5276d6db2c1
Jerod <jijiudong@droi.com>
2017-09-10 17:46:14 +0800

    Initial Commit

---
+++ TailorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TailorDemo
+//
+//  Created by JerodJi on 2017/9/10.
+//  Copyright © 2017年 Jerod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
